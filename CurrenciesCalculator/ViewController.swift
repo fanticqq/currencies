326f95d6b7a4cc6358443216ced6929adc98340a
i.zarubin <i.zarubin@corp.mail.ru>
2018-10-05 12:34:45 +0300

    Initial Commit

---
+++ CurrenciesCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrenciesCalculator
+//
+//  Created by i.zarubin on 05/10/2018.
+//  Copyright © 2018 i.zarubin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
